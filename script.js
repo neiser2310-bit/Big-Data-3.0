c5aa66703ed8ddd102a2694b5cb04836d505c493
neiser2310-bit <neiser2310@gmail.com>
2026-05-22 01:00:00 -0500

    Fix variable name and update table insertion logic

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const formulario = document.getElementById("formulario");
-const tabla = document.getElementById("tablaDatos");
+
+const tablaDatos = document.getElementById("tablaDatos");
+
 
 formulario.addEventListener("submit", function(e){
 
     e.preventDefault();
 
     let nombre = document.getElementById("nombre").value;
+
     let apellido = document.getElementById("apellido").value;
+
     let edad = document.getElementById("edad").value;
+
     let correo = document.getElementById("correo").value;
 
-    tabla.innerHTML += `
+
+    tablaDatos.innerHTML += `
+
         <tr>
+
             <td>${nombre}</td>
+
             <td>${apellido}</td>
+
             <td>${edad}</td>
+
             <td>${correo}</td>
+
         </tr>
+
     `;
 
+
     formulario.reset();
+
 });
